9fc7b73543db05c5ca056dc1291ba96bc4a88c90
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-09-13 16:43:06 -0400

    mutation changes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,7 @@ const resolvers = {
 
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({})
+                const userData = await User.findOne({_id: context.user._id })
                     .select('-__v -password')
                     .populate('books');
 
@@ -46,13 +46,13 @@ const resolvers = {
 
         },
 
-        saveBook: async (parent, args, context) => {
+        saveBook: async (parent, { bookData }, context) => {
             if (context.user) {
                 // const savedBooks = await Book.findOneandUpdate({ ...args, username: context.user.username });
 
                 await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $push: { savedBooks: book._id } },
+                    { $push: { savedBooks: bookData } },
                     { new: true }
                 );
 
@@ -62,13 +62,13 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
 
-        deleteBook: async (parent, args, context) => {
+        deleteBook: async (parent, {bookId}, context) => {
             if (context.user) {
                 // const saveBdooks = await Book.findOneandUpdate({ ...args, username: context.user.username });
 
                 await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: book._id } },
+                    { $pull: { savedBooks: {bookId: bookId} } },
                     { new: true }
                 );
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,12 +9,47 @@ type User {
   username: String
   email: String
   bookCount: Int
+  savedBooks: [Book]
 }
+
+type Book {
+  bookId: String
+  authors: [String]
+  description: String
+  image: String 
+  link: String
+  title: String
+}
+
+input SavedBook {
+  bookId: String
+  authors: [String]
+  description: String
+  image: String 
+  link: String
+  title: String
+}
+
 type Query {
   me: User
   users: [User]
   user(username: String!): User
+  books(username: String): [Book]
+  book(bookId: String): Book
 }
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(bookData: BookInfo!): User
+  deleteBook(bookId: ID!): User
+}
+
 `;
 
 // export the typeDefs
